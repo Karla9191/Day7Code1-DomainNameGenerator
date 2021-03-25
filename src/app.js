b287dd498bd4f7e03000d1eb1f693d9f0da10fb9
Karla9191 <marietha2014@gmail.com>
2021-03-25 04:12:21 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["The", "our"];
+
+  let adj = ["great", "big"];
+
+  let noun = ["jogger", "racoon"];
+
+  let ext = [".com", ".es", ".net", ".us", ".io"];
+
+  let RanPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+
+  let RanAdj = adj[Math.floor(Math.random() * adj.length)];
+
+  let RanNoun = noun[Math.floor(Math.random() * noun.length)];
+
+  let RanExt = ext[Math.floor(Math.random() * ext.length)];
+
+  let totalFrase = RanPronoun + RanAdj + RanNoun + RanExt;
+
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log("Hello Karla desde console.log!" + pronoun[i]);
+    document.getElementById("excuseF").innerHTML = totalFrase;
+  }
+
+  document.getElementById("excuseF").innerHTML = totalFrase;
 };
